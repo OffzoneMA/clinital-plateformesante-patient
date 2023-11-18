e8f7451448ec35119356957e61ccf2fe78a1d35c
solaymanebenhaddi <heddoudi.mohamed@gmail.com>
2023-11-18 13:58:09 +0000

    fix webpack ...

--- src/components/searchBarDoc/SearchBarDoc.jsx
+++ src/components/searchBarDoc/SearchBarDoc.jsx
@@ -70,7 +70,8 @@ function SearchBarDoc({ setRandomX, comp }) {
       dispatch({ type: "CITY_FETCH" });
       try {
         const citys = await axios.get(
-          "https://apidb.clinital.io/api/ville/allvilles"
+          "http://localhost:8080/api/ville/allvilles"
+          //"https://apidb.clinital.io/api/ville/allvilles"
         );
         dispatch({ type: "CITY_SUCCESS", payload: citys.data });
       } catch (error) {
